378a68e14fc79078fab9f1ad5839216a2e675cf0
YD <ydk8355856700@gmail.com>
2025-11-17 18:12:56 +0900

    인트로 화면과 메인화면 만들었습니다.

---
+++ app/menu/page.tsx
@@ -0,0 +1,19 @@
+export default function MenuPage() {
+  return (
+    <div className="page-bg">
+      <div className="kiosk">
+
+        <div className="button-zone">
+          <button className="kiosk-button">
+            일반 재활용<br />방법 안내
+          </button>
+
+          <button className="kiosk-button">
+            대형 폐기물<br />수거 신청
+          </button>
+        </div>
+
+      </div>
+    </div>
+  );
+}
