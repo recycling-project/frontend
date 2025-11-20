b67a86ee801c88ef764a2e3c9c0d9126b1413454
SY <rsy2169@gmail.com>
2025-11-20 16:40:17 +0900

    "use client";
    export const dynamic = "force-dynamic";
    export const fetchCache = "force-no-store";
    export const revalidate = 0;

--- app/Waste/loading/page.tsx
+++ app/Waste/loading/page.tsx
@@ -1,5 +1,9 @@
 "use client";
 
+export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
+export const revalidate = 0;
+
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 import { useSearchParams } from "next/navigation";
--- app/Waste/result/page.tsx
+++ app/Waste/result/page.tsx
@@ -1,5 +1,9 @@
 "use client";
 
+export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
+export const revalidate = 0;
+
 import { useSearchParams } from "next/navigation";
 
 export default function WasteResult() {
