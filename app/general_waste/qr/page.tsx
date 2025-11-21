49d15e89e4bb5aaaa7f39f47b77ffadc12de1bb1
YD <ydk8355856700@gmail.com>
2025-11-21 15:47:26 +0900

    화면 크기 비율을 꽉 채움 으로 수정, 키보드 자판 입력시 버튼에 밀리는 거 수정.했습니다.

--- app/general_waste/qr/page.tsx
+++ app/general_waste/qr/page.tsx
@@ -1,19 +1,31 @@
 "use client";
-
+import { useRouter } from "next/navigation";
 import { QRCodeCanvas } from "qrcode.react";
 
 export default function QRPage() {
+  const router = useRouter();
   const uploadUrl = "http://<서버주소>:8080/recycle/upload-mobile";
 
   return (
     <div className="page-bg">
       <div className="kiosk">
 
-        <h2>휴대폰으로 사진을 업로드하세요</h2>
+        {/* 상단 뒤로가기 버튼 */}
+        <img
+          src="/back_icon.png"
+          alt="뒤로가기"
+          className="back-btn"
+          onClick={() => router.back()}
+        />
 
-        <QRCodeCanvas value={uploadUrl} size={260} />
+        <h2 className="qr-title">휴대폰으로 사진을 업로드하세요</h2>
 
-        <p>QR을 휴대폰으로 스캔하세요</p>
+        <div className="qr-wrapper">
+          <QRCodeCanvas value={uploadUrl} size={260} />
+        </div>
+
+        <p className="qr-guide">QR을 휴대폰으로 스캔하세요</p>
+
       </div>
     </div>
   );
