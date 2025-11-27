0a584b3cbca3e0945b05fc692c5b8a0fdfd33695
YD <ydk8355856700@gmail.com>
2025-11-27 10:42:25 +0900

    ff

--- app/general_waste/result/page.tsx
+++ app/general_waste/result/page.tsx
@@ -2,7 +2,7 @@
 
 export const dynamic = "force-dynamic";
 export const fetchCache = "force-no-store";
-export const revalidate = 0;
+// export const revalidate = 0;
 
 import { useSearchParams } from "next/navigation";
 
