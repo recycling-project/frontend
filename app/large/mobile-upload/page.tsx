4c2ad4ac7b1475f4ab8049a51900264d4ae7e110
SY <rsy2169@gmail.com>
2025-12-01 17:33:18 +0900

    모바일업로드에서 이후 경로 수정

--- app/large/mobile-upload/page.tsx
+++ app/large/mobile-upload/page.tsx
@@ -18,7 +18,7 @@ export default function LargeMobileUploadPage() {
 
       try {
         const res = await fetch(
-          `${process.env.NEXT_PUBLIC_API_URL}/recycle/mobile-upload-large`,
+          `${process.env.NEXT_PUBLIC_API_URL}/large/mobile-upload`,
           {
             method: "POST",
             headers: { "Content-Type": "application/json" },
